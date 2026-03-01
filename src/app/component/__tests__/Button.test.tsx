1196615e8ef3f00af42052a7d28789becc32d258
phamlequocphi-1609 <phamlequocphi16092001@gmail.com>
2026-03-01 16:32:45 +0700

    add unit test

---
+++ src/app/component/__tests__/Button.test.tsx
@@ -0,0 +1,8 @@
+import {render, screen} from '@testing-library/react';
+import Button from '../Button';
+
+
+test("render button", () => {
+  render(<Button />)
+  expect(screen.getByText("Hello")).toBeInTheDocument()
+})
